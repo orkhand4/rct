a5197b24742bd343996a18ec3fa5f3e00ce6382d
Orkhand4 <orkhand4@mail.ru>
2024-11-10 13:33:30 +0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React, {useRef} from 'react'
+
+const App = () => {
+  
+  const x = useRef()
+
+  const demo = () => {
+    console.log(x);
+    console.log(x.current);
+    console.log(x.current.innerText);
+    
+    
+  }
+
+  return (
+    <div>
+      <button ref={x} onClick={demo}>goster</button>
+    </div>
+  )
+}
+
+export default App
